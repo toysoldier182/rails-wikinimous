790f65584493f7bb1155e99c3d86047c3eead681
juancarlosasensio <jasensio@ufm.edu>
2020-02-18 16:58:00 +0000

    Create Articles controller, actions, views and Bootstrap styles

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    @article.create
+    redirect_to article_path(params[:id])
+  end
+end
